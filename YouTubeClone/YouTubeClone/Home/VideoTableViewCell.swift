cf06bf8ccc2bcc22994a1b24499ecf3bcb6febce
Rumeysa Tan <tanrumeysa93@gmail.com>
2022-08-07 18:05:26 +0300

    video cell design added on storyboard

---
+++ YouTubeClone/YouTubeClone/Home/VideoTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  VideoTableViewCell.swift
+//  YouTubeClone
+//
+//  Created by Rumeysa TAN on 7.08.2022.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+
+extension UIImageView {
+    private func maskCircle(anyImage : UIImage) {
+        self.contentMode = UIView.ContentMode.scaleAspectFill
+        self.layer.cornerRadius = self.frame.height / 2
+        self.layer.masksToBounds = false
+        self.clipsToBounds = true
+
+       self.image = anyImage
+    }
+}
