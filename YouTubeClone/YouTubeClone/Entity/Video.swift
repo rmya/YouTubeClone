41e8ebe838f3906b602ff65e65665a698f2eb141
Rumeysa Tan <tanrumeysa93@gmail.com>
2022-08-08 09:07:50 +0300

    channel image key added

--- YouTubeClone/YouTubeClone/Entity/Video.swift
+++ YouTubeClone/YouTubeClone/Entity/Video.swift
@@ -14,13 +14,18 @@ struct Video : Decodable {
     let view: String
     let date: String
     let video_key: String
+    let channel_image_key: String
     
-    var image_url: URL {
+    var imageUrl: URL {
         return URL(string: "https://img.youtube.com/vi/\(video_key)/0.jpg")!
     }
     
-    var video_url : URL {
+    var videoUrl : URL {
         return URL(string: "https://www.youtube.com/embed/\(video_key)")!
+    }
+    
+    var channelImageUrl: URL {
+        return URL(string: "https://yt3.ggpht.com/\(channel_image_key)=s176-c-k-c0x00ffffff-no-rj")!
     }
     
     var shortDescription: String {
@@ -31,3 +36,12 @@ struct Video : Decodable {
         return "\(view) - \(date)"
     }
 }
+
+/*
+ https://yt3.ggpht.com/  ytc/AMLnZu9zF4fL89PQTjFdOy-Pl77CSZMs3QmUb0aHDpqo-g      =s176-c-k-c0x00ffffff-no-rj  GSA
+ https://yt3.ggpht.com/  ytc/AMLnZu89XFESjT_G02aVXK7odDFD5Q6ZUJh3sabRYMT_zw      =s176-c-k-c0x00ffffff-no-rj    dmclub
+ https://yt3.ggpht.com/  ytc/AMLnZu9DajdQkN_8ForNXwwhSr-LW3e_UAdozfjZUm4c        =s176-c-k-c0x00ffffff-no-rj     anatolianrock
+ https://yt3.ggpht.com/  ytc/AMLnZu-t8XClBHfrJjQsC-1deajGgJiDo6pK_a2LgUZ8        =s176-c-k-c0x00ffffff-no-rj      breakoreal
+ https://yt3.ggpht.com/  46Me_fbbbklqDQBuEIS0O1S00n1h8FTsYiE8n3FBRf7oWZpJDgQkQ7QgwVkv4r5STck597UHng  =s176-c-k-c0x00ffffff-no-rj  POETS
+ https://yt3.ggpht.com/  7cvusshBDq1UDcpYCyLFO-x1tqdoJXn7Pa6OcPvG1DeX2Dr1pFkOyeTWY1-taB4OdcX5EWobHA  =s176-c-k-c0x00ffffff-no-rj   Karsu
+ */
